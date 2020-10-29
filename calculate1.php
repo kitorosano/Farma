fc3f1d6943915a4e8f80ae1b1fcf11f6267c9391
Kitorosano <rosanoesteban2@gmail.com>
2020-10-29 00:35:16 -0300

    fixing cantidad

--- calculate1.php
+++ calculate1.php
@@ -77,10 +77,10 @@ $farmaData = $consul_GeoLocation->fetchAll();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>calculando.</title>
 
-  <form id="form1" action="calculate2.php" method="POST">
+  <!-- <form id="form1" action="calculate2.php" method="POST"> -->
     <input name="farmaciaDistancia" id="farmaciaDistancia" hidden value="" />
     <input name="casaDireccion" id="casaDireccion" hidden value="" />
-  </form>
+  <!-- </form> -->
 
 
   <script src="https://js.api.here.com/v3/3.1/mapsjs-core.js" type="text/javascript" charset="utf-8"></script>
@@ -146,7 +146,7 @@ $farmaData = $consul_GeoLocation->fetchAll();
       }
 
       document.getElementById("farmaciaDistancia").value = JSON.stringify(farmaciaDistancia);
-      document.getElementById("form1").submit();
+      // document.getElementById("form1").submit();
     };
 
     start();
